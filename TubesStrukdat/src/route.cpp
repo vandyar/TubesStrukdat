147b403acbdc0f3fb0bafd479a7527ad86a2fb76
lauradjasmine <djasminelaura@gmail.com>
2025-12-02 21:18:28 +0700

    Update route.cpp

--- TubesStrukdat/src/route.cpp
+++ TubesStrukdat/src/route.cpp
@@ -1,14 +1,48 @@
 #include <iostream>
+#include "route.h"
 
 using namespace std;
 
-int main()
-{
-	cout << " hallo vandyar \n";
-	cout << " apa kamu sedang mengerjakan Tubes? \n";
-	cout << " hallo laura";
-	
-
+	adrRute newRute(int id) {
+		adrRute R = new Rute;
+        R->id = id;
+        R->next = nullptr;
+        return R;
 }
 
+adrRute findRute(adrRute head, int id) {
+    adrRute p = head;
+    while (p) {
+        if (p->id == id) return p;
+        p = p->next;
+    }
+    return nullptr;
+}
 
+void insertRute(adrRute &head, int id) {
+    adrRute R = newRute(id);
+    R->next = head;
+    head = R;
+}
+
+void deleteRute(adrRute &head, int id) {
+    adrRute cur = head, prev = nullptr;
+    while (cur && cur->id != id) {
+        prev = cur;
+        cur = cur->next;
+    }
+    if (!cur) return;
+    if (!prev) head = cur->next;
+    else prev->next = cur->next;
+    delete cur;
+}
+
+void showAllRute(adrRute headRute) {
+    adrRute r = headRute;
+    cout << "Daftar Rute: ";
+    while (r) {
+        cout << r->id << " ";
+        r = r->next;
+    }
+    cout << endl;
+}
