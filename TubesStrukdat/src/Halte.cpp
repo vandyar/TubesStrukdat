805198b52fb7acd058676eabef8c64071bc9b33a
van <gm5856476@gmail.com>
2025-12-02 14:35:09 +0700

    Add files via upload

---
+++ TubesStrukdat/src/Halte.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	cout << "hallo laura" << endl;
+	cout << "apa kabar?";
+
+	return 0;
+}
---
+++ TubesStrukdat/src/route.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << " hallo vandyar \n";
+	cout << " apa kamu sedang mengerjakan Tubes? \n";
+}
