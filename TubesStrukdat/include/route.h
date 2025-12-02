9bb78454d530532cefd71a791d488076ecf5d589
van <gm5856476@gmail.com>
2025-12-02 16:23:49 +0700

    Update route.h

---
+++ TubesStrukdat/include/route.h
@@ -0,0 +1,28 @@
+#ifndef ROUTE_H_INCLUDED
+#define ROUTE_H_INCLUDED
+
+
+#include<iostream>
+
+using namespace std;
+
+struct Halte {
+  string NamaHalte;
+  rute *FirstRute;
+  Halte *next;
+
+}
+
+struct Rute {
+  int id* rute
+  Rute *next 
+
+}
+
+typedef Halte adrHalte;
+typedef Rute adrRute;
+
+
+adrHalte newHalte(string name)
+
+
